ae8676072e06b71e093054946543f97a3e6c54ba
raeth <andres@moadw.com>
2024-09-03 18:40:51 -0500

    refactor(battleship.cpp): remove unused copy2DArray function for code cleanliness feat(battleship.cpp): add functionality to check if space is available before placing ship feat(battleship.cpp): add support for ships to be marked as placed to prevent re-placement feat(battleship.cpp): add functionality to check if all ships of a player are placed feat(battleship.cpp): add functionality for players to take turns and check hits feat(battleship.cpp): add functionality to check if all ships of a player are sunk feat(battleship.cpp): implement main game logic for Battleship game with two players

--- battleship.cpp
+++ battleship.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define SIZE 9
 
@@ -8,6 +7,7 @@ typedef struct {
     int size;
     int x, y;
     char direction; 
+    int placed;
 } Ship;
 
 typedef struct {
@@ -16,14 +16,6 @@ typedef struct {
     Ship ship2;
     Ship ship3;
 } Player;
-
-void copy2DArray(char dest[SIZE][SIZE], char src[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            dest[i][j] = src[i][j];
-        }
-    }
-}
 
 void initializeBoard(char board[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
@@ -34,7 +26,14 @@ void initializeBoard(char board[SIZE][SIZE]) {
 }
 
 void printBoard(char board[SIZE][SIZE]) {
+    printf("   ");
     for (int i = 0; i < SIZE; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d  ", i);
         for (int j = 0; j < SIZE; j++) {
             printf("%c ", board[i][j]);
         }
@@ -42,7 +41,27 @@ void printBoard(char board[SIZE][SIZE]) {
     }
 }
 
+int isSpaceAvailable(char board[SIZE][SIZE], Ship ship) {
+    for (int i = 0; i < ship.size; i++) {
+        if (ship.direction == 'H') {
+            if (ship.x + i >= SIZE || board[ship.y][ship.x + i] != '.') {
+                return 0;
+            }
+        } else {
+            if (ship.y + i >= SIZE || board[ship.y + i][ship.x] != '.') {
+                return 0; 
+            }
+        }
+    }
+    return 1;
+}
+
 void placeShip(char board[SIZE][SIZE], Ship ship) {
+    if (!isSpaceAvailable(board, ship)) {
+        printf("No se puede colocar el barco en la posición (%d, %d) con dirección %c. Espacio ocupado o fuera del tablero.\n", ship.x, ship.y, ship.direction);
+        return;
+    }
+    
     for (int i = 0; i < ship.size; i++) {
         if (ship.direction == 'H') {
             board[ship.y][ship.x + i] = 'S';
@@ -62,9 +81,9 @@ void clearShip(char board[SIZE][SIZE], Ship ship) {
     }
 }
 
-void moveShip(char board[SIZE][SIZE], Ship *ship, char input, Player *player) {
-    clearShip(board, *ship);
-    
+void moveShip(char board[SIZE][SIZE], Ship *ship, char input) {
+    clearShip(board, *ship); 
+
     switch (input) {
         case 'w':
             if (ship->y > 0) ship->y--;
@@ -86,33 +105,127 @@ void moveShip(char board[SIZE][SIZE], Ship *ship, char input, Player *player) {
             if (ship->direction == 'V' && ship->y + ship->size > SIZE) ship->y = SIZE - ship->size;
             break;
         case 'g':
-            player->ship1 = *ship;
-            printf("Barco guardado para %s en posición (%d, %d) con dirección %c\n", player->name, player->ship1.x, player->ship1.y, player->ship1.direction);
+            ship->placed = 1;
+            printf("Barco guardado en posición (%d, %d) con dirección %c\n", ship->x, ship->y, ship->direction);
             break;
     }
 
-    placeShip(board, *ship);
+    if (isSpaceAvailable(board, *ship)) {
+        placeShip(board, *ship);
+    } else {
+        printf("La nueva posición del barco está ocupada o fuera del tablero. Inténtelo de nuevo.\n");
+        clearShip(board, *ship); 
+    }
+}
+
+void placeShipsForPlayer(Player *player, char board[SIZE][SIZE]) {
+    while (1) {
+        printBoard(board);
+        printf("%s, use 'w', 'a', 's', 'd' para mover el barco, 'r' para rotar, 'g' para guardar ubicación.\n", player->name);
+
+        char input = getchar();
+        getchar(); 
+
+        if (!player->ship1.placed) {
+            moveShip(board, &player->ship1, input);
+        } else if (!player->ship2.placed) {
+            moveShip(board, &player->ship2, input);
+        } else if (!player->ship3.placed) {
+            moveShip(board, &player->ship3, input);
+        } else {
+            printf("Todos los barcos de %s están colocados.\n", player->name);
+            break;
+        }
+
+        system("clear"); 
+    }
+}
+
+int checkHit(char board[SIZE][SIZE], int x, int y) {
+    if (board[y][x] == 'S') {
+        board[y][x] = 'X';
+        return 1; 
+    } else {
+        board[y][x] = 'O';
+        return 0;
+    }
+}
+
+void takeTurn(Player *attacker, Player *defender, char board[SIZE][SIZE]) {
+    int x, y;
+    printf("%s, ingresa las coordenadas para disparar (x y): ", attacker->name);
+    scanf("%d %d", &x, &y);
+    
+    if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+        printf("Coordenadas inválidas. Intenta de nuevo.\n");
+        return;
+    }
+
+    int hit = checkHit(board, x, y);
+    if (hit) {
+        printf("¡Impacto en (%d, %d)!\n", x, y);
+    } else {
+        printf("Fallo en (%d, %d).\n", x, y);
+    }
+}
+
+int allShipsSunk(Player *player, char board[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == 'S') {
+                return 0; 
+            }
+        }
+    }
+    return 1; 
 }
 
 int main() {
-    char board[SIZE][SIZE];
-    Ship ship1 = {4, 0, 0, 'H'}; 
-    Ship ship2 = {3, 0, 0, 'H'}; 
-    Ship ship3 = {2, 0, 0, 'H'}; 
-    
-    Player player1 = {"player1"};
+    char board1[SIZE][SIZE];
+    char board2[SIZE][SIZE];
+    char board1Shots[SIZE][SIZE];
+    char board2Shots[SIZE][SIZE];
 
-    initializeBoard(board);
-    placeShip(board, ship1);
+    Player player1 = {"Player1", {4, 0, 0, 'H', 0}, {3, 0, 0, 'H', 0}, {2, 0, 0, 'H', 0}};
+    Player player2 = {"Player2", {4, 0, 0, 'H', 0}, {3, 0, 0, 'H', 0}, {2, 0, 0, 'H', 0}};
 
-    while (1) {
-        printBoard(board);
-        printf("Use 'w', 'a', 's', 'd' para mover el barco, 'r' para rotar, 'g' para guardar ubicacion.\n");
+    initializeBoard(board1);
+    initializeBoard(board2);
+    initializeBoard(board1Shots);
+    initializeBoard(board2Shots);
 
-        char input = getchar();
-        moveShip(board, &ship1, input, &player1);
+    printf("Jugador 1: ¡Coloca tus barcos!\n");
+    placeShipsForPlayer(&player1, board1);
+åç
+    system("clear"); 
 
-        system("clear"); 
+    printf("Jugador 2: ¡Coloca tus barcos!\n");
+    placeShipsForPlayer(&player2, board2);
+
+    system("clear"); 
+    int gameOn = 1;
+    while (gameOn) {
+        printf("%s, tu tablero de disparos:\n", player1.name);
+        printBoard(board1Shots);
+        printf("%s, elige dónde disparar:\n", player1.name);
+        takeTurn(&player1, &player2, board2);
+
+        if (allShipsSunk(&player2, board2)) {
+            printf("%s ha ganado. Todos los barcos de %s han sido hundidos.\n", player1.name, player2.name);
+            gameOn = 0;
+            break;
+        }
+
+        printf("%s, tu tablero de disparos:\n", player2.name);
+        printBoard(board2Shots);
+        printf("%s, elige dónde disparar:\n", player2.name);
+        takeTurn(&player2, &player1, board1);
+
+        if (allShipsSunk(&player1, board1)) {
+            printf("%s ha ganado. Todos los barcos de %s han sido hundidos.\n", player2.name, player1.name);
+            gameOn = 0;
+            break;
+        }
     }
 
     return 0;
